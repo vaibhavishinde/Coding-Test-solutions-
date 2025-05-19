4506f02ff1eef8bdb60d7e2d6259e61846137026
vaibhavishinde <105842361+vaibhavishinde@users.noreply.github.com>
2025-05-20 01:22:57 +0530

    Update Program-4.java

--- Program-4.java
+++ Program-4.java
@@ -1,10 +1,8 @@
-import java.util.*;
-
 public class Program4 {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
-        Map<Integer, Integer> result = new HashMap<>();
 
+        System.out.print("{");
         for (int i = 1; i <= 9; i++) {
             int count = 0;
             for (int num : numbers) {
@@ -12,9 +10,11 @@ public class Program4 {
                     count++;
                 }
             }
-            result.put(i, count);
+            System.out.print(i + ": " + count);
+            if (i < 9) {
+                System.out.print(", ");
+            }
         }
-
-        System.out.println(result);
+        System.out.println("}");
     }
 }
